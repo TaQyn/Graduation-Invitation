630df26826d0c9a5b3b477d1b3d4fc12d7159abf
TaQyn <146752007+TaQyn@users.noreply.github.com>
2025-11-09 11:42:23 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,12 @@
+console.log("Script is running!");
 const params = new URLSearchParams(window.location.hash.slice(1));
+console.log("Params:", params.toString());
 const name = params.get("name") || "to all my friends";
+console.log("Name:", name);
 document.querySelector(".invite-name").textContent = `Dear ${name} 🌸`;
-
 
 
 document.getElementById('mapBtn').addEventListener('click', () => {
     window.open('https://maps.app.goo.gl/EEKoGgwdMPJNjq568', '_blank');
 });
+
