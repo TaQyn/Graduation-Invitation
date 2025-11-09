cc210ae7754f6dadaa0113e52e400db5926da807
TaQyn <146752007+TaQyn@users.noreply.github.com>
2025-11-09 09:54:43 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,20 @@
+// Lấy tên từ URL
+const params = new URLSearchParams(window.location.search);
+const name = params.get("name") || "";
 
+// Thay chỗ … bằng tên
+if (name) {
+    const h1 = document.querySelector(".invite-name");
+    h1.textContent = `Dear ${name} 🌸`;
+}
 
 
 document.getElementById('mapBtn').addEventListener('click', () => {
     window.open('https://maps.app.goo.gl/EEKoGgwdMPJNjq568', '_blank');
+});
+
+
+document.getElementById('mapBtn').addEventListener('click', () => {
+    window.open('https://maps.app.goo.gl/EEKoGgwdMPJNjq568', '_blank');
+
 });
