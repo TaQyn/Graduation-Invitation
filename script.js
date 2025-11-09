9664dafcfabda46cd11eeda312bf34fd3813adbe
TaQyn <146752007+TaQyn@users.noreply.github.com>
2025-11-09 11:18:24 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,4 @@
-document.addEventListener("DOMContentLoaded", function () {
-    // Lấy tên người nhận từ URL
-    const params = new URLSearchParams(window.location.search);
-    const name = params.get("name") || "";
 
-    if (name) {
-        const h1 = document.querySelector(".invite-name");
-        if (h1) {
-            h1.textContent = `Dear ${name} 🌸`;
-        }
-    }
 
     // Nút mở Google Maps
     const mapBtn = document.getElementById("mapBtn");
@@ -18,3 +8,4 @@ document.addEventListener("DOMContentLoaded", function () {
         });
     }
 });
+
