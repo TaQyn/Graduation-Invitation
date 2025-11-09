64b906a9609b72961b8f30161fcef6022f5e2609
TaQyn <146752007+TaQyn@users.noreply.github.com>
2025-11-09 10:07:06 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,17 +1,20 @@
-document.addEventListener("DOMContentLoaded", function() {
+document.addEventListener("DOMContentLoaded", function () {
     // Lấy tên người nhận từ URL
     const params = new URLSearchParams(window.location.search);
     const name = params.get("name") || "";
 
-    if(name) {
+    if (name) {
         const h1 = document.querySelector(".invite-name");
-        if(h1) {
+        if (h1) {
             h1.textContent = `Dear ${name} 🌸`;
         }
     }
 
-
-document.getElementById('mapBtn').addEventListener('click', () => {
-    window.open('https://maps.app.goo.gl/EEKoGgwdMPJNjq568', '_blank');
+    // Nút mở Google Maps
+    const mapBtn = document.getElementById("mapBtn");
+    if (mapBtn) {
+        mapBtn.addEventListener("click", function () {
+            window.open("https://maps.app.goo.gl/EEKoGgwdMPJNjq568", "_blank");
+        });
+    }
 });
-
